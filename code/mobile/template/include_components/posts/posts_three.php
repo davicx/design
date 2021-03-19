8127235bc6213c76a7c4baef16c6a5f5a5bb2857
vasquez_d <vasquezd@oregonstate.edu>
2021-03-19 15:43:07 -0700

    updates

---
+++ code/mobile/template/include_components/posts/posts_three.php
@@ -0,0 +1,44 @@
+<div class="">
+    <div class="post-three">
+		<p class = "post-text lato"> 120 </p>
+		<img src="images/square.png" alt="Wall Post Photo" class = "post-photo-image">
+		
+	</div>
+    <div class="post-three">
+		<img src="images/square.png" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+	   <div class="post-three">
+		<img src="images/square.png" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_1.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_2.png" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_3.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_4.png" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_5.png" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_6.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+    <div class="post-three">
+		<img src="images/image_7.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+	    <div class="post-three">
+		<img src="images/image_8.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+	<div class="post-three">
+		<img src="images/image_9.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>	
+    <div class="post-three">
+		<img src="images/image_10.jpg" alt="Wall Post Photo" class = "post-photo-image">
+	</div>
+		
+</div>
